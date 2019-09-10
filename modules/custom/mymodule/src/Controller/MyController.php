5060a75cca174ae5162b9559adbaed0463e9a1ed
Yogesh Shukla <shuklayogesh065@gmail.com>
2019-09-10 23:22:58 +0530

    custom module

--- modules/custom/mymodule/src/Controller/MyController.php
+++ modules/custom/mymodule/src/Controller/MyController.php
@@ -18,20 +18,36 @@ class MyController {
 
   public function subscribe ( $cid )
   {
-    $node = Node::create([
-      'type'  => 'enrollment',
-      'title' => 'Subscribed',
-      'body'  => 'abc',
-      'field_course' =>[
-        'target_id' => $cid
-      ],
-      'field_student' =>[
-        'target_id' => \Drupal::currentUser()->id()
-      ],
-      'uid' => \Drupal::currentUser()->id(),
-    ]);
-    $node->save();
-    drupal_set_message(t('You have successfully subscribed'), 'status');
+    $values = \Drupal::entityQuery('node')
+      ->condition('field_course', $cid)
+      ->condition('uid', \Drupal::currentUser()->id())
+      ->execute();
+    if(!empty($values)){
+      $result = \Drupal::entityQuery("node")
+      ->condition('field_course', $cid)
+      ->condition('uid', \Drupal::currentUser()->id())
+      ->execute();
+  
+      $storage_handler = \Drupal::entityTypeManager()->getStorage("node");
+      $entities = $storage_handler->loadMultiple($result);
+      $storage_handler->delete($entities);
+      drupal_set_message(t('You have successfully Unsubscribed'), 'status');       
+    } else {
+      $node = Node::create([
+        'type'  => 'enrollment',
+        'title' => 'Subscribed',
+        'body'  => 'abc',
+        'field_course' =>[
+          'target_id' => $cid
+        ],
+        'field_student' =>[
+          'target_id' => \Drupal::currentUser()->id()
+        ],
+        'uid' => \Drupal::currentUser()->id(),
+      ]);
+      $node->save();
+      drupal_set_message(t('You have successfully subscribed'), 'status');
+    }
     return new RedirectResponse("/lms/courses");
   }
 }
